54f5c23914d7e4e88af7d5818415afe121ebbd50
Kirill Dolgopolov <70063811+KirillDolgopolov@users.noreply.github.com>
2023-10-23 21:56:47 +0200

    Add files via upload

---
+++ S05T01N01DolgopolovKirill/src/main/java/cat/itacademy/barcelonactiva/dolgopolov/kirill/s05/t01/n01/controllers/SucursalController.java
@@ -0,0 +1,4 @@
+package cat.itacademy.barcelonactiva.dolgopolov.kirill.s05.t01.n01.controllers;
+
+public class SucursalController {
+}
---
+++ S05T01N01DolgopolovKirill/src/test/java/cat/itacademy/barcelonactiva/dolgopolov/kirill/s05/t01/n01/S05T01N01DolgopolovKirillApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.dolgopolov.kirill.s05.t01.n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01DolgopolovKirillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
